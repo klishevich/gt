6b0798912923634085a383bf2f0267107b36b453
Michael Klishevich <busation@gmail.com>
2017-08-25 08:26:55 +0300

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def courses
+  end
+
+  def mentors
+  end
+
+  def faq
+  end
+
+  def hiring_students
+  end
+
+  def corporate_workshop
+  end
+
+  def become_instructor
+  end
+end
